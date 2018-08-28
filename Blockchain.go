7ea29112a60ce32b5e3cf3a87eae03cfda171263
Rainsong <eder.gabriel.felix@gmail.com>
2018-08-28 20:51:53 -0300

    wip ProofOfWork

--- Blockchain.go
+++ Blockchain.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
+	"math"
 	"strconv"
 	"time"
 )
@@ -18,6 +19,7 @@ type Block struct {
 	Hash         []byte
 	Content      string
 	Timestamp    int64
+	Nonce        int64
 }
 
 func (b *Block) generateHash() {
@@ -30,6 +32,35 @@ func (b *Block) generateHash() {
 	b.Hash = h[:]
 }
 
+func GenerateProofOfWork(b Block) int64 {
+	nonce := int64(1)
+	for {
+		b.Nonce = nonce
+		if b.isValid() {
+			return nonce
+		}
+		nonce += 1
+	}
+}
+
+func (b *Block) isValid() bool {
+	block_bytes := bytes.Join([][]byte{
+		b.Hash,
+		[]byte(strconv.FormatInt(b.Nonce, 10)),
+	}, []byte{})
+	h := sha256.Sum256(block_bytes)
+	var sum int64 = 0
+	for i := 0; i < len(h); i++ {
+		sum += int64(i)
+	}
+	fmt.Println(len(h))
+	fmt.Println(sum)
+	if sum < math.MaxInt64>>50 {
+		return true
+	}
+	return false
+}
+
 func NewBlock(content string, prevBlockHash []byte) *Block {
 	block := new(Block)
 	block.Content = content
@@ -37,6 +68,9 @@ func NewBlock(content string, prevBlockHash []byte) *Block {
 	block.Timestamp = time.Now().Unix()
 
 	block.generateHash()
+
+	block.Nonce = GenerateProofOfWork(*block)
+
 	return block
 }
 
@@ -47,14 +81,19 @@ func MakeBlockchain() *Blockchain {
 	return chain
 }
 
+func (bc *Blockchain) mineBlock() {
+	bc.Blocks = append(bc.Blocks, NewBlock("abc", bc.Blocks[len(bc.Blocks)-1].Hash))
+}
+
 func main() {
 	blockchain := MakeBlockchain()
 
 	for i := 0; i < 5; i++ {
-		blockchain.Blocks = append(blockchain.Blocks, NewBlock("abc", []byte{123, 3}))
+		blockchain.mineBlock()
+		a, _ := json.Marshal(*(blockchain.Blocks[len(blockchain.Blocks)-1]))
+		fmt.Println(string(a))
 	}
 
 	s, _ := json.Marshal(*blockchain)
 	fmt.Println(string(s))
-
 }
